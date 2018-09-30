1937895d8d04bcfdb14ecb6f282a365d5cf7035b
rverbytskyi <r.j.verbytskyi@gmail.com>
2018-09-30 22:34:05 +0300

    Fix AppWrapper files structure

---
+++ js/containers/AppWrapper/AppWrapper.container.js
@@ -0,0 +1,24 @@
+import { connect } from 'react-redux'
+import { setUI } from '../../actions/ui'
+
+import AppWrapper from './AppWrapper.component'
+import { disconnected, connected } from '../../actions/network'
+
+function mapDispatchToProps(dispatch) {
+  return {
+    setWidth: (value) => {
+      dispatch(setUI({ name: 'width', value }))
+    },
+    setHeight: (value) => {
+      dispatch(setUI({ name: 'height', value }))
+    },
+    disconnected: () => {
+      dispatch(disconnected())
+    },
+    connected: () => {
+      dispatch(connected())
+    },
+  }
+}
+
+export default connect(null, mapDispatchToProps)(AppWrapper)
--- js/containers/AppWrapper/index.js
+++ js/containers/AppWrapper/index.js
@@ -1,24 +1,3 @@
-import { connect } from 'react-redux'
-import { setUI } from '../../actions/ui'
+import AppWrapper from './AppWrapper.container'
 
-import AppWrapper from './AppWrapper'
-import { disconnected, connected } from '../../actions/network'
-
-function mapDispatchToProps(dispatch) {
-  return {
-    setWidth: (value) => {
-      dispatch(setUI({ name: 'width', value }))
-    },
-    setHeight: (value) => {
-      dispatch(setUI({ name: 'height', value }))
-    },
-    disconnected: () => {
-      dispatch(disconnected())
-    },
-    connected: () => {
-      dispatch(connected())
-    },
-  }
-}
-
-export default connect(null, mapDispatchToProps)(AppWrapper)
+export default AppWrapper
